1c5cba44cc986f11880a6b84b74699dbf4b8a844
Potandr1977 <andrew.potapov.77@inbox.ru>
2019-07-15 18:54:41 +0300

    refactoring

--- PersonDiary.React.EFCore/ClientApp/src/components/Person.jsx
+++ PersonDiary.React.EFCore/ClientApp/src/components/Person.jsx
@@ -11,6 +11,7 @@ class Person extends Component {
     constructor(props) {
         super(props);
         this.onChangePersonName = this.onChangePersonName.bind(this);
+        this.onChangePersonSurName = this.onChangePersonSurName.bind(this);
         this.save = this.save.bind(this);
 
         const id = parseInt(this.props.match.params.id, 10) || 0;
@@ -19,23 +20,13 @@ class Person extends Component {
 
     onChangePersonName(e) {
         this.props.person.name = e.target.value;
+        
+    }
+    onChangePersonSurName(e) {
+        this.props.person.surname = e.target.value;
     }
     save(e) {
         this.props.savePerson(this.props.person);
-        /*
-        fetch('api/upload/' + this.state.id, {
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state.upload)
-        })//.then(response => (response) ? response.json() : "" )
-            .then(data => {
-                alert('saved');//this.setState({ forecasts: data, loading: false });
-            });
-            */
-        alert('22');
     }
     componentWillReceiveProps(nextProps) {
         // This method runs when incoming props (e.g., route params) change
@@ -44,53 +35,55 @@ class Person extends Component {
 
 
     static renderLifeEvents(lifeevents) {
-        return (
-            <table className='table'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {lifeevents.map(lifeevent =>
-                        <tr key={lifeevent.id}>
-                            <td>{lifeevent.name}</td>
-                            <td>{lifeevent.eventdate}</td>
+        if (lifeevents)
+            return (
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Date</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
-        );
+                    </thead>
+                    <tbody>
+                        {lifeevents.map(lifeevent =>
+                            <tr key={lifeevent.id}>
+                                <td>{lifeevent.name}</td>
+                                <td>{lifeevent.eventdate}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            )
+        else return "";
     }
 
     render() {
 
-        let i = 0;
-        let contents = !this.props.lifeevent
-            ? <p><em>Loading...</em></p>
-            : Person.renderLifeEvent(this.props.person.lifeevents);
+        if (this.props.person) {
+            let contents = !this.props.person
+                ? <p><em>Loading...</em></p>
+                : Person.renderLifeEvents(this.props.person.lifeevents);
 
-        let Name = this.props.person ? this.props.person.name : "";
-        let Surname = this.props.person ? this.props.person.surname : "";
-        return (
-            <div>
-                <h1>Person</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input type="text" value={Name} onChange={this.onChangePersonName} className="form-control" />
+            return (
+                <div>
+                    <h1>Person</h1>
+                    <p>This component demonstrates fetching data from the server.</p>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" value={this.props.person.name} onChange={this.onChangePersonName} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Surname</label>
+                        <input type="text" value={this.props.person.surname} onChange={this.onChangePersonSurName} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="button" value="Сохранить" onClick={this.save} className="btn btn-success" />
+                    </div>
+                    {contents}
                 </div>
-                <div className="form-group">
-                    <label>Surname</label>
-                    <input type="text" value={Surname} className="form-control" />
-                </div>
-                <div className="form-group">
-                    <input type="button" value="Сохранить" onClick={this.save} className="btn btn-success" />
-                </div>
-                {contents}
-            </div>
-        );
+            )
+        }
+        else return "loading...";
     }
 }
 
